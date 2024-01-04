26f352f0fef817f04d698095b1736dbaeab8a79c
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2024-01-04 15:25:16 +0100

    add pytest

--- test_amazon_search.py
+++ test_amazon_search.py
@@ -5,23 +5,25 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# specify the path of the driver
-service = Service('/Users/chiaradigi/Documents/python_selenium_automation_testing/chromedriver')
-# create a new instance of the driver
-driver = webdriver.Chrome(service=service)
 
-driver.get('https://www.amazon.com/')
+def test_amazon_search():   
+    # specify the path of the driver
+    service = Service('/Users/chiaradigi/Documents/python_selenium_automation_testing/chromedriver')
+    # create a new instance of the driver
+    driver = webdriver.Chrome(service=service)
 
-# wait for the search bar to be loaded
-wait = WebDriverWait(driver, 10)
-# find the search bar
-search = wait.until(EC.presence_of_element_located((By.ID, 'twotabsearchtextbox')))
-# input text and click enter
-search.send_keys('dress',Keys.ENTER)
+    driver.get('https://www.amazon.com/')
 
-expected_text = '"dress"'
-actual_text = driver.find_element(By.XPATH,"//span[@class='a-color-state a-text-bold']").text
+    # wait for the search bar to be loaded
+    wait = WebDriverWait(driver, 10)
+    # find the search bar
+    search = wait.until(EC.presence_of_element_located((By.ID, 'twotabsearchtextbox')))
+    # input text and click enter
+    search.send_keys('dress',Keys.ENTER)
 
-assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text}'
-# close the browser
-driver.quit() 
+    expected_text = '"dress"'
+    actual_text = driver.find_element(By.XPATH,"//span[@class='a-color-state a-text-bold']").text
+
+    assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text}'
+    # close the browser
+    driver.quit() 
